9672efdd15a20ca7b6cbaf3f6b77daa540607f43
Jelmer <j.cnossen@gmail.com>
2019-10-04 15:59:39 +0200

    example update

--- python/simflux_example.py
+++ python/simflux_example.py
@@ -1,15 +1,11 @@
+# Process a simulated dataset with the simflux pipeline
 import sys
 sys.path.append("simflux")
 
 import simflux.process_tiff as process_tiff
-import numpy as np
 import urllib.request
 
 import os
-
-pixelsize = 65 # nm/pixel
-
-mod = np.zeros((6,5))
 
 config = { 
    'roisize':9,
@@ -22,10 +18,8 @@ config = {
    'gain':1
 }
 
-sigma_sim = 1.663
-
 # Simple worm-like chain simulated model
-path = 'object_wlc_15.tif' 
+path = './object_wlc_15.tif' 
 
 if not os.path.exists(path):
     print('Beginning file download with urllib2...')
@@ -33,9 +27,10 @@ if not os.path.exists(path):
     url = 'http://homepage.tudelft.nl/f04a3/object_wlc_15.tif'
     urllib.request.urlretrieve(url, path)
 
-    
-
+pixelsize = 65 # nm/pixel
 spotfilter = ('moderror', 0.012)
+
+# Define the layout of frames (Here, 3 phase steps for 2 angles are used)
 pattern_frames = [[0,2,4],[1,3,5]] 
 
 process_tiff.process(path, config, 
